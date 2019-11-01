d7569f98d82382bf05ae0ae403942b13ffe4ee3f
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-01 17:46:07 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var addPlayersScreen = document.querySelector(".add-players-screen");
+var rulesScreen = document.querySelector(".rules-screen");
+var header = document.querySelector("header");
+
+window.onload = rulesScreen.classList.add("hidden");
+window.onload = addPlayersScreen.classList.add("hidden");
+window.onload = header.classList.add("hidden");
