b2da1e6a7de4a9c2c24734f641c61dfce0f40083
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-04 14:26:58 -0700

    Refactor 2

--- deck.js
+++ deck.js
@@ -5,7 +5,7 @@ class Deck {
     this.selectedCards = [];
     this.matches = 0;
 
-var card1 = ({name: 'card1', matchId: 01, isMatched: false})
+var card1 = ({name: 'card1', matchId: 01, isMatched: false, imageFront: url(), imageBack:()})
 var card2 = ({name: 'card2', matchId: 02, isMatched: false})
 
   shuffle() {
