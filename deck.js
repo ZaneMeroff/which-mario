513400da80b4db529b67ecc9c9f618b0be21c98b
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-04 09:07:44 -0700

    Setup methods

--- deck.js
+++ deck.js
@@ -1,20 +1,22 @@
-// class Deck {
-//   constructor() {
-//     this.cards = card;
-//     this.matchedCards = matchedCards;
-//     this.selectedCards = selectedCards;
-//     this.matches = matches;
-//
-//
-//   shuffle() {
-//   }
-//
-//   checkSelectedCards() {
-//   }
-//
-//   moveToMatched() {
-//   }
-//
-//   }
-//
-// }
+class Deck {
+  constructor(cards) {
+    this.cards = cards || [];
+    this.matchedCards = [];
+    this.selectedCards = [];
+    this.matches = 0;
+
+var card1 = ({name: 'card1', matchId: 01, isMatched: false})
+var card2 = ({name: 'card2', matchId: 02, isMatched: false})
+
+  shuffle() {
+  }
+
+  checkSelectedCards() {
+  }
+
+  moveToMatched() {
+  }
+
+  }
+
+}
