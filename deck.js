346a6631e10574cd9d5ffac49c8365b0c1ba5591
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-04 21:27:35 -0700

    Create shuffle function

--- deck.js
+++ deck.js
@@ -1,12 +1,21 @@
 class Deck {
   constructor(cards) {
     this.cards = cards || [];
+    this.shuffle();
     this.matchedCards = [];
     this.selectedCards = [];
     this.matches = 0;
   }
 
   shuffle() {
+    var j, x, i;
+    for (i = this.cards.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = x;
+    }
+    return this.cards;
   }
 
   checkSelectedCards() {
