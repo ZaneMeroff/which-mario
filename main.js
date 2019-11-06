af5b093646f777ed7731bb4496c01d03820b62b5
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-05 21:44:34 -0700

    Styel new game button

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ var p2span = document.querySelector(".p2-span");
 var cardArea = document.querySelector(".card-area");
 var p1name = document.querySelector(".p1-name");
 var p2name = document.querySelector(".p2-name");
+var startNewGameButton = document.querySelector(".start-new-game-button");
 var player1Matches = 0;
 var winningMessage = "";
 var startTime = 0;
@@ -31,9 +32,18 @@ var deck = new Deck(instanciateCards());
 
 window.onload = onPageLoad();
 
+startNewGameButton.addEventListener("click", startNewGame);
 cardArea.addEventListener("click", buttonConditionals);
 playGameButton.addEventListener("click", advanceToRulesScreen);
 playGameButton2.addEventListener("click", advanceToGameBoard);
+
+function startNewGame() {
+  deck = new Deck(instanciateCards());
+  document.querySelector(".card-area").innerHTML = "";
+  advanceToGameBoard();
+  player1Matches = 0;
+  upDateP1Score();
+}
 
 function onPageLoad() {
   rulesScreen.classList.add("hidden");
@@ -67,9 +77,10 @@ function advanceToGameBoard() {
 }
 
 function createCardsOnDOM() {
+  document.querySelector(".card-area").innerHTML = "";
   for (var i = 0; i < deck.cards.length; i++) {
   document.querySelector(".card-area").innerHTML += `
-   <div class="card card${deck.cards[i].cardId}">
+   <div class="card card${deck.cards[i].cardId} grid${i}">
      <img class ="c${deck.cards[i].cardId} card-back" src="./images/mario_card_back.jpg" alt="mario bricks">
      <img class ="c${deck.cards[i].cardId} card-face" src=${deck.cards[i].imageFront} alt="blue flower">
    </div>`
@@ -148,13 +159,15 @@ function timeCheck() {
   var min = Math.floor(totalTime / 60);
   var sec = Math.round (totalTime % 60);
   winningMessage = `You finshed in ${min}min(s) and ${sec}sec(s)!`;
+  return totalTime;
 }
 
 function displayYouWin() {
   if (deck.matchedCards.length === deck.cards.length) {
-    timeCheck();
+    var playersTime = timeCheck();
     document.querySelector(".card-area").innerHTML = `
     <p class="winner-message-l1">you win ${inputPlayer1.value}!</p>
     <p class="winner-message-l2">${winningMessage}</p>`
+    localStorage.setItem(inputPlayer1.value, JSON.stringify(playersTime));
   }
 }
